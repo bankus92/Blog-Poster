f111f4e7768b829d1bb3c4d9fb50aedfcd9cb6f8
bankus92 <justinkbaird92@gmail.com>
2026-01-19 15:31:39 -0500

    'Title' input and 'body' input now outputs the value to the 'output' element after pressing the 'post' button

--- script.js
+++ script.js
@@ -4,11 +4,17 @@ const title = document.getElementById("titleInput");
 //Blog body input
 const body = document.getElementById("bodyInput");
 
+//Output
+const titleOut = document.getElementById("titleOutput");
+const bodyOut = document.getElementById("bodyOutput");
+
 //"Post" button
 const btn = document.getElementById("generateBtn");
 btn.addEventListener("click", () => {
-  console.log("clicked");
+  //Gets value of 'Title' & 'Body'
+  const titleValue = title.value;
+  const bodyValue = body.value;
+  //Outputs value of 'Title' & 'Body'
+  titleOut.textContent = titleValue;
+  bodyOut.textContent = bodyValue;
 });
-
-//Output
-const output = document.getElementById("outputHtml");
