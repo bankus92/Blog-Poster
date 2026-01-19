07c64aeba9dcb9634ae62ec133afdfaa7bfd5956
bankus92 <justinkbaird92@gmail.com>
2026-01-19 14:39:52 -0500

    Basic HTML skeleton, initial JS script, prepping for DOM manipulation

---
+++ script.js
@@ -0,0 +1,14 @@
+//Blog title input
+const title = document.getElementById("titleInput");
+
+//Blog body input
+const body = document.getElementById("bodyInput");
+
+//"Post" button
+const btn = document.getElementById("generateBtn");
+btn.addEventListener("click", () => {
+  console.log("clicked");
+});
+
+//Output
+const output = document.getElementById("outputHtml");
